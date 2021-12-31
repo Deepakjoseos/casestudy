fb8de0dcd2aed800226f7f746511c1b2c83703c0
Deepakjoseos <93700189+Deepakjoseos@users.noreply.github.com>
2021-12-31 14:04:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.get('/',function(req,res){
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log("Server Ready on "+PORT);
+app.listen(5000,()=>{
+    console.log("Server Ready on 3000");
 });
